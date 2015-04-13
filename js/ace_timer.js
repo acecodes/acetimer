778d638a0fff50f421e83e8d79b25c84c9ad36a1
Ace <ace.github@gmail.com>
2015-04-12 20:25:16 -0700

    Refactored and added use strict statement

--- js/ace_timer.js
+++ js/ace_timer.js
@@ -1,48 +1,60 @@
 var $ = jQuery.noConflict();
 
-$(function () {
+$(function() {
+    'use strict';
 
-$('#timer').countdown({until: 0, format:'MS', 
-    onExpiry: breakTime}); 
- 
-$('#timerStart').click(function() { 
-    $('#timer').countdown('option', {until: 1500, format:'MS'});  
-}); 
- 
- 
-$('#pauseButton').click(function() { 
-    var pause = $(this).text() === 'Pause'; 
-    $(this).text(pause ? 'Resume' : 'Pause'); 
-    $('#timer').countdown(pause ? 'pause' : 'resume'); 
-}); 
+    function breakTime() {
+
+        $('#breakTimerInfo').toggle(0, function() {
+            $.playSound('audio/break');
+        });
 
 
-function breakTime() {
-	
-  $( "#breakTimerInfo").toggle( 0, function() { $.playSound('audio/break');
-  });
 
-  
+        $('#startingBlock').toggle(0, function() {});
 
-  $( "#startingBlock").toggle( 0, function() {
-  });
+        $('#breakTimer').countdown({
+            until: 300,
+            onExpiry: breakTime,
+            format: 'MS'
+        });
 
-	$('#breakTimer').countdown({until: 300,  
-    onExpiry: breakTime, format:'MS'}); 
- 
-	$('#breakTimerStart').click(function() { 
-    	$('#breakTimer').countdown('option', {until: 300, format:'MS'});  
-	}); 
- 
- 
-	$('#breakPauseButton').click(function() { 
-	    var pause = $(this).text() === 'Pause'; 
-	    $(this).text(pause ? 'Resume' : 'Pause'); 
-	    $('#breakTimer').countdown(pause ? 'pause' : 'resume'); 
-	}); 
+        $('#breakTimerStart').click(function() {
+            $('#breakTimer').countdown('option', {
+                until: 300,
+                format: 'MS'
+            });
+        });
 
-	}; 
 
-$(".alert").alert()
+        $('#breakPauseButton').click(function() {
+            var pause = $(this).text() === 'Pause';
+            $(this).text(pause ? 'Resume' : 'Pause');
+            $('#breakTimer').countdown(pause ? 'pause' : 'resume');
+        });
+
+    }
+
+    $('#timer').countdown({
+        until: 0,
+        format: 'MS',
+        onExpiry: breakTime
+    });
+
+    $('#timerStart').click(function() {
+        $('#timer').countdown('option', {
+            until: 1500,
+            format: 'MS'
+        });
+    });
+
+
+    $('#pauseButton').click(function() {
+        var pause = $(this).text() === 'Pause';
+        $(this).text(pause ? 'Resume' : 'Pause');
+        $('#timer').countdown(pause ? 'pause' : 'resume');
+    });
+
+
+    $('.alert').alert();
 });
-
